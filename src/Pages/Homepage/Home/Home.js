3e0f0b5ca607e277689c98b49527d7444f703aa4
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-04-18 20:39:02 +0600

    Extra One Section Added

--- src/Pages/Homepage/Home/Home.js
+++ src/Pages/Homepage/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
+import WhyHireMe from '../WhyHireMe/WhyHireMe';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Services></Services>
+            <WhyHireMe></WhyHireMe>
         </div>
     );
 };
---
+++ src/Pages/Homepage/WhyHireMe/WhyHireMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WhyHireMe = () => {
+    return (
+        <div className='container'>
+            <h3 className='text-center my-4'>My Specialties</h3>
+            <h2 className='text-center'>Why Choose <span className='text-danger'>Me</span> </h2>
+            <p className='mt-3'>Tour guides require strong communication and public speaking skills to convey information effectively. In addition, active listening skills ensure tourists get your full attention. This skill also helps you understand any questions you receive during a tour."</p>
+            <p><strong>❝Over the years, I have been guiding tourists and gaining experience, which has enabled me to guide you in the right direction.I have also worked tirelessly on my communication skills and teamwork skills, which I can help you in any unexpected environment, if you take my service I will try my best to give you.❞</strong></p>
+        </div>
+    );
+};
+
+export default WhyHireMe;
